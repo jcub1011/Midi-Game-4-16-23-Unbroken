47399dbb53090823274f9dcb329b7cf8d92c4026
Jacob <me@here.com>
2023-05-14 20:31:50 -0400

    Working on scubbing overlay.

--- Assets/UI Toolkit/Scrubbing Overlay/ScrubbingOverlay.cs
+++ Assets/UI Toolkit/Scrubbing Overlay/ScrubbingOverlay.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,13 @@ using UnityEngine.UIElements;
 public class ScrubbingOverlay : MonoBehaviour
 {
     public VisualElement VisualElement { get; protected set; }
+    public Slider PlaybackScrubber { get; protected set; }
+    private bool _sliderInitalized = false;
 
-    void InitOverlay()
+    void GetElements()
     {
-        VisualElement = transform.GetComponent<UIDocument>().rootVisualElement;
+        VisualElement = GetComponent<UIDocument>().rootVisualElement;
+        PlaybackScrubber = VisualElement.Q("Scrubber") as Slider;
     }
 
     public bool Visible
@@ -26,6 +30,20 @@ public class ScrubbingOverlay : MonoBehaviour
 
     private void Start()
     {
-        InitOverlay();
+        GetElements();
+    }
+
+    public void InitSlider(float minPlaybackTime, float maxPlaybackTime)
+    {
+        _sliderInitalized = true;
+        PlaybackScrubber.lowValue = minPlaybackTime;
+        PlaybackScrubber.highValue = maxPlaybackTime;
+        PlaybackScrubber.value = minPlaybackTime;
+    }
+
+    public void UpdateSlider(float playbackTime)
+    {
+        if (!_sliderInitalized) return;
+        PlaybackScrubber.value = playbackTime;
     }
 }
